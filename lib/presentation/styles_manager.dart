71af0e8315cd8a26e4c3e2f6370c9501b9bf7f37
flutterdevtaha <59686887+flutterdevtaha@users.noreply.github.com>
2022-09-07 11:22:16 +0300

    Create styles_manager.dart

---
+++ lib/presentation/styles_manager.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/painting.dart';
+import 'package:flutter_app/presentation/font_manager.dart';
+
+TextStyle _getTextStyle(double fontSize, FontWeight fontWeight, Color color) {
+  return TextStyle(
+    fontSize: fontSize,
+    fontWeight: fontWeight,
+    color: color,
+    fontFamily: FontConstants.fontFamily,
+  );
+}
+
+// light style
+TextStyle getLightStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(fontSize, FontWeightManager.light, color);
+}
+
+// regular style
+TextStyle getRegularStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(fontSize, FontWeightManager.regular, color);
+}
+
+// medium style
+TextStyle getMediumStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(fontSize, FontWeightManager.medium, color);
+}
+
+// semibold style
+TextStyle getSemiBoldStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(fontSize, FontWeightManager.semiBold, color);
+}
+
+// bold style
+TextStyle getBoldtStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(fontSize, FontWeightManager.bold, color);
+}
