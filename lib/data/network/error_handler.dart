f5b388d25ae720dd2410360369d54279a03c1f9a
Taha Saad <flutterdev.taha.saad@gmail.com>
2022-09-14 19:14:56 +0300

    adding the datasource errors

---
+++ lib/data/network/error_handler.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: constant_identifier_names
+
+enum DataSource {
+  SUCCESS,
+  NO_CONTENT,
+  BAD_REQUEST,
+  UNAUTHORIZED,
+  FORBIDDEN,
+  NOT_FOUND,
+  INTERNAL_SERVER_ERROR,
+  CONNECT_TIMEOUT,
+  CANCEL,
+  RECIEVE_TIMEOUT,
+  SEND_TIMEOUT,
+  CACHE_ERROR,
+  NO_INTERNET_CONNECTION,
+}
