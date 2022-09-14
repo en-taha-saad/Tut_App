5c550c4b8cbe78d7baaac1b4ee137226c258e34a
Taha Saad <flutterdev.taha.saad@gmail.com>
2022-09-14 18:24:54 +0300

    creating repository with failure and success

---
+++ lib/domain/repository/repository.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_app/data/network/models/failure.dart';
+import 'package:flutter_app/data/network/models/loginrequest.dart';
+import 'package:flutter_app/domain/models/login/authentication.dart';
+
+abstract class Repository {
+  Future<Either<Failure, Authentication>> login(LoginRequest loginRequest);
+}
+ 
