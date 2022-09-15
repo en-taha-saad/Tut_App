44fa450d4f24f4933894381b0dff0a8a144b8ddd
Taha Saad <flutterdev.taha.saad@gmail.com>
2022-09-15 13:52:46 +0300

    apply error handle on api repository

---
+++ lib/data/network/error_handler/api_internal_status.dart
@@ -0,0 +1,6 @@
+// ignore_for_file: constant_identifier_names
+
+class ApiInternalStatus {
+  static const int SUCCESS = 0;
+  static const int FAILURE = 1;
+}
--- lib/data/repository/repository_impl.dart
+++ lib/data/repository/repository_impl.dart
@@ -1,5 +1,11 @@
 import 'package:flutter_app/data/data_sources/remote/remote_data_source.dart';
 import 'package:flutter_app/data/mappers/authentication_response_mapper.dart';
+import 'package:flutter_app/data/network/error_handler/api_internal_status.dart';
+import 'package:flutter_app/data/network/error_handler/datasource_enum.dart';
+import 'package:flutter_app/data/network/error_handler/datasource_extension.dart';
+import 'package:flutter_app/data/network/error_handler/error_handler.dart';
+import 'package:flutter_app/data/network/error_handler/response_code.dart';
+import 'package:flutter_app/data/network/error_handler/response_messages.dart';
 import 'package:flutter_app/data/network/internet_checker/network_info.dart';
 import 'package:flutter_app/domain/models/login/authentication.dart';
 import 'package:flutter_app/data/network/models/loginrequest.dart';
@@ -18,27 +24,26 @@ class RepositoryImpl implements Repository {
       LoginRequest loginRequest) async {
     if (await _networkInfo.isConnected) {
       // its connected to internet so we can call the api
-      final response = await _remoteDataSource.login(loginRequest);
-      if (response.status == 0) {
-        // success reutrn either right
-        return Right(response.toDomain());
-      } else {
-        // failure return either left business error
-        return Left(
-          Failure(
-            409,
-            response.message ?? "Something went wrong",
-          ),
-        );
+      try {
+        final response = await _remoteDataSource.login(loginRequest);
+        if (response.status == ApiInternalStatus.SUCCESS) {
+          // success reutrn either right
+          return Right(response.toDomain());
+        } else {
+          // failure return either left business error
+          return Left(
+            Failure(
+              ApiInternalStatus.FAILURE,
+              response.message ?? ResponseMessage.DEFAULT,
+            ),
+          );
+        }
+      } catch (error) {
+        return Left(ErrorHandler.handle(error).failure);
       }
     } else {
       // its not connected to internet so return a failure
-      return Left(
-        Failure(
-          501,
-          "please check your internet connection",
-        ),
-      );
+      return Left(DataSource.NO_INTERNET_CONNECTION.getFailure());
     }
   }
 }
