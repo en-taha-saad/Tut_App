b884eb944b402b7aab59ee5ac51b75d98a425931
Taha Saad <flutterdev.taha.saad@gmail.com>
2022-09-16 05:54:50 +0300

    completing login usecase

---
+++ lib/domain/usecase/login_usecase/login_usecase.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_app/data/network/models/failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_app/data/network/models/loginrequest.dart';
+import 'package:flutter_app/domain/models/login/authentication.dart';
+import 'package:flutter_app/domain/repository/repository.dart';
+import 'package:flutter_app/domain/usecase/base_usecase.dart';
+import 'package:flutter_app/domain/usecase/login_usecase/login_usecase_input.dart';
+
+class LoginUseCase implements BaseUseCase<LoginUseCaseInput, Authentication> {
+  final Repository _repository;
+  LoginUseCase(this._repository);
+  @override
+  Future<Either<Failure, Authentication>> execute(
+      LoginUseCaseInput input) async {
+    return await _repository.login(
+      LoginRequest(input.email, input.password),
+    );
+  }
+}
---
+++ lib/domain/usecase/login_usecase/login_usecase_input.dart
@@ -0,0 +1,5 @@
+class LoginUseCaseInput {
+  String email;
+  String password;
+  LoginUseCaseInput(this.email, this.password);
+}
