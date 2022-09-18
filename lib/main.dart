d9edda3928cca2336aeac5dc9162f22247f3fd69
flutterdevtaha <59686887+flutterdevtaha@users.noreply.github.com>
2022-09-18 15:22:39 +0300

    adding init login module

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/app/dependency_injections/init_app_module.dart';
 
 import 'app/app.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await initAppModule();
+void main() {
   runApp(MyApp());
 }
