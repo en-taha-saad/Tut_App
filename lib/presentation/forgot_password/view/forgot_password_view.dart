50462ad634cde09d5cc8cda03c13e722b9575f12
flutterdevtaha <59686887+flutterdevtaha@users.noreply.github.com>
2022-09-20 15:26:33 +0300

    bind viewmodel to view

--- lib/presentation/forgot_password/view/forgot_password_view.dart
+++ lib/presentation/forgot_password/view/forgot_password_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/app/dependency_injections/init_app_module.dart';
+import 'package:flutter_app/presentation/forgot_password/viewmodel/forgot_password_viewmodel.dart';
 
 class ForgotPasswordView extends StatefulWidget {
   const ForgotPasswordView({Key? key}) : super(key: key);
@@ -9,6 +11,25 @@ class ForgotPasswordView extends StatefulWidget {
 }
 
 class _ForgotPasswordViewState extends State<ForgotPasswordView> {
+  final ForgotPasswordViewModel _viewModel =
+      instance<ForgotPasswordViewModel>();
+
+  _bind() {
+    _viewModel.start();
+  }
+
+  @override
+  void initState() {
+    _bind();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _viewModel.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
--- lib/presentation/forgot_password/viewmodel/forgot_password_viewmodel.dart
+++ lib/presentation/forgot_password/viewmodel/forgot_password_viewmodel.dart
@@ -4,6 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/domain/usecase/forgot_password_usecase/forgot_password_usecase.dart';
 import 'package:flutter_app/domain/usecase/forgot_password_usecase/forgot_password_usecase_input.dart';
 import 'package:flutter_app/presentation/common/freezed_data_classes.dart';
+import 'package:flutter_app/presentation/common/state_render/states/content_state.dart';
+import 'package:flutter_app/presentation/common/state_render/states/error_state.dart';
+import 'package:flutter_app/presentation/common/state_render/states/loading_state.dart';
+import 'package:flutter_app/presentation/common/state_render/states/state_renderer_type.dart';
 import 'package:flutter_app/presentation/forgot_password/viewmodel/forgot_password_viewmodel_inputs.dart';
 import 'package:flutter_app/presentation/forgot_password/viewmodel/forgot_password_viewmodel_outputs.dart';
 import '../../base/base_viewmodel.dart';
@@ -15,35 +19,40 @@ class ForgotPasswordViewModel extends BaseViewModel
   var forgotPasswordObject = ForgotPasswordObject("");
 
   @override
-  void start() {}
+  void start() {
+    // view model should tell view, please show content state
+    inputState.add(ContentState());
+  }
+
   @override
   void dispose() {
+    super.dispose();
     _emailController.close();
     _areSentVerificationStreamController.close();
   }
 
   @override
   forgotPassword() async {
-    // inputState.add(
-    //   LoadingState(stateRendererType: StateRendererType.popupLoadingState),
-    // );
+    inputState.add(
+      LoadingState(stateRendererType: StateRendererType.popupLoadingState),
+    );
     (await _forgotPasswordUseCase.execute(
       ForgotPasswordUseCaseInput(forgotPasswordObject.email),
     ))
         .fold(
       (failure) {
         // left -> failure
-        // inputState.add(
-        //   ErrorState(
-        //     StateRendererType.popupErrorState,
-        //     failure.message,
-        //   ),
-        // );
+        inputState.add(
+          ErrorState(
+            StateRendererType.popupErrorState,
+            failure.message,
+          ),
+        );
         debugPrint("failure = ${failure.message}");
       },
       (data) {
         // right -> success (data)
-        // inputState.add(ContentState());
+        inputState.add(ContentState());
         debugPrint("data = ${data.message}");
       },
     );
