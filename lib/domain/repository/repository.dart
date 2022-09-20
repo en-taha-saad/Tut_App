940034fbf0d0a9fa8b85ffecb6dca7e8179fe5cc
flutterdevtaha <59686887+flutterdevtaha@users.noreply.github.com>
2022-09-20 12:19:59 +0300

    adding forgot password request

--- lib/domain/repository/repository.dart
+++ lib/domain/repository/repository.dart
@@ -1,8 +1,13 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_app/data/network/models/failure.dart';
+import 'package:flutter_app/data/network/models/forgot_password_request.dart';
 import 'package:flutter_app/data/network/models/loginrequest.dart';
+import 'package:flutter_app/domain/models/forgot_password/forgot_password.dart';
 import 'package:flutter_app/domain/models/login/authentication.dart';
 
 abstract class Repository {
   Future<Either<Failure, Authentication>> login(LoginRequest loginRequest);
+  Future<Either<Failure, ForgotPassword>> forgotPassword(
+    ForgotPasswordRequest forgotPasswordRequest,
+  );
 }
