3184a102c3c143dd97e7bc4f1333b3c8c4670a6c
Taha Saad <flutterdev.taha.saad@gmail.com>
2022-09-22 14:24:34 +0300

    making remote data source and its implementation for the function register

--- lib/data/data_sources/remote/remote_data_source.dart
+++ lib/data/data_sources/remote/remote_data_source.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_app/data/network/models/loginrequest.dart';
+import 'package:flutter_app/data/network/models/register_request.dart';
 import 'package:flutter_app/data/responses/forget_password_responses/forget_password_responses.dart';
 import 'package:flutter_app/data/responses/login_responses/login_responses.dart';
 
 abstract class RemoteDataSource {
   Future<AuthenticationResponse> login(LoginRequest loginRequest);
   Future<ForgotPasswordResponse> forgotPassword(String email);
+  Future<AuthenticationResponse> register(RegisterRequest registerRequest);
 }
--- lib/data/data_sources/remote/remote_data_source_impl.dart
+++ lib/data/data_sources/remote/remote_data_source_impl.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_app/data/data_sources/remote/remote_data_source.dart';
 import 'package:flutter_app/data/network/app_api.dart';
 import 'package:flutter_app/data/network/models/loginrequest.dart';
+import 'package:flutter_app/data/network/models/register_request.dart';
 import 'package:flutter_app/data/responses/forget_password_responses/forget_password_responses.dart';
 import 'package:flutter_app/data/responses/login_responses/login_responses.dart';
 
@@ -19,5 +20,19 @@ class RemoteDataSourceImpl implements RemoteDataSource {
   @override
   Future<ForgotPasswordResponse> forgotPassword(String email) async {
     return await _appServiceClient.forgotPassword(email);
+  }
+
+  @override
+  Future<AuthenticationResponse> register(
+    RegisterRequest registerRequest,
+  ) async {
+    return await _appServiceClient.register(
+      registerRequest.userName,
+      registerRequest.countryMobileCode,
+      registerRequest.mobileNumber,
+      registerRequest.email,
+      registerRequest.password,
+      registerRequest.profilePicture,
+    );
   }
 }
