f4f5b05d7287c41fc437e6cc25f66f815bd3e8ca
Taha Saad <flutterdev.taha.saad@gmail.com>
2022-09-22 14:19:08 +0300

    putting the api to the repository with making the request

--- lib/domain/repository/repository.dart
+++ lib/domain/repository/repository.dart
@@ -1,9 +1,13 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_app/data/network/models/failure.dart';
 import 'package:flutter_app/data/network/models/loginrequest.dart';
+import 'package:flutter_app/data/network/models/register_request.dart';
 import 'package:flutter_app/domain/models/login/authentication.dart';
 
 abstract class Repository {
   Future<Either<Failure, Authentication>> login(LoginRequest loginRequest);
   Future<Either<Failure, String>> forgotPassword(String email);
+  Future<Either<Failure, Authentication>> register(
+    RegisterRequest registerRequest,
+  );
 }
