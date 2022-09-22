557dd4f50bdc0cd457deb7a9e697c555bbe2047f
Taha Saad <flutterdev.taha.saad@gmail.com>
2022-09-22 14:12:47 +0300

    adding api request to appServiceClient

--- lib/data/network/app_api.dart
+++ lib/data/network/app_api.dart
@@ -17,4 +17,14 @@ abstract class AppServiceClient {
 
   @POST("/customer/forgotPassword")
   Future<ForgotPasswordResponse> forgotPassword(@Field("email") String email);
+
+  @POST("/customer/register")
+  Future<AuthenticationResponse> register(
+    @Field("user_name") String userName,
+    @Field("country_mobile_code") String countryMobileCode,
+    @Field("mobile_number") String mobileNumber,
+    @Field("email") String email,
+    @Field("password") String password,
+    @Field("profile_picture") String profilePicture,
+  );
 }
