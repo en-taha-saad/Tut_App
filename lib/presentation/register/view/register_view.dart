75690a7edc3b9e804fa42d4efcc623e4e27a2493
Taha Saad <flutterdev.taha.saad@gmail.com>
2022-09-22 18:45:31 +0300

    adding controllers and listen to them

--- lib/presentation/register/view/register_view.dart
+++ lib/presentation/register/view/register_view.dart
@@ -3,6 +3,12 @@ import 'package:flutter_app/app/dependency_injections/init_app_module.dart';
 import 'package:flutter_app/presentation/common/state_render/states/flow_state.dart';
 import 'package:flutter_app/presentation/register/viewmodel/register_viewmodel.dart';
 import 'package:flutter_app/presentation/common/state_render/states/flow_state_extension.dart';
+import 'package:flutter_app/presentation/resources/other_managers/assets_manager.dart';
+import 'package:flutter_app/presentation/resources/other_managers/color_manager.dart';
+import 'package:flutter_app/presentation/resources/other_managers/strings_manager.dart';
+import 'package:flutter_app/presentation/resources/routes_manager/routes.dart';
+import 'package:flutter_app/presentation/resources/values_manager/app_padding.dart';
+import 'package:flutter_app/presentation/resources/values_manager/app_size.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
@@ -14,9 +20,27 @@ class RegisterView extends StatefulWidget {
 
 class _RegisterViewState extends State<RegisterView> {
   final RegisterViewModel _viewModel = instance<RegisterViewModel>();
+  final TextEditingController _userNameController = TextEditingController();
+  final TextEditingController _mobileNumberController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _userPasswordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   _bind() {
     _viewModel.start();
+    _userNameController.addListener(() {
+      _viewModel.setUserName(_userNameController.text);
+    });
+    _mobileNumberController.addListener(() {
+      _viewModel.setMobileNumber(_mobileNumberController.text);
+    });
+    _emailController.addListener(() {
+      _viewModel.setEmail(_emailController.text);
+    });
+    _userPasswordController.addListener(() {
+      _viewModel.setPassword(_userPasswordController.text);
+    });
+
   }
 
   @override
@@ -47,6 +71,95 @@ class _RegisterViewState extends State<RegisterView> {
   }
 
   Widget _getContentWidget() {
-    return Container();
+    return Scaffold(
+      backgroundColor: ColorManager.white,
+      body: Container(
+        padding: const EdgeInsets.only(top: AppPadding.p100),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                const Center(
+                  child: Image(
+                    image: AssetImage(
+                      ImageAssets.splashLogo,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: AppPadding.p28,
+                    right: AppPadding.p28,
+                    top: AppPadding.p28,
+                  ),
+                  child: StreamBuilder<bool>(
+                    stream: _viewModel.outputIsUserNameValid,
+                    builder: (context, snapshot) {
+                      return TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        controller: _userNameController,
+                        decoration: InputDecoration(
+                          hintText: AppStrings.username,
+                          labelText: AppStrings.username,
+                          errorText: _viewModel.,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: AppPadding.p28,
+                    right: AppPadding.p28,
+                  ),
+                  child: StreamBuilder<bool>(
+                    stream: _viewModel.,
+                    builder: (context, snapshot) {
+                      return SizedBox(
+                        width: double.infinity,
+                        height: AppSize.s40,
+                        child: ElevatedButton(
+                          onPressed: (snapshot.data ?? false)
+                              ? () => _viewModel.login()
+                              : null,
+                          child: const Text(AppStrings.login),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: AppPadding.p8,
+                    left: AppPadding.p28,
+                    right: AppPadding.p28,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushNamed(
+                            context,
+                            Routes.loginRoute
+                          );
+                        },
+                        child: Text(
+                          AppStrings.login,
+                          style: Theme.of(context).textTheme.titleMedium,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
