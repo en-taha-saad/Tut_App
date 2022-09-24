ebede8516ae7651f5190519dc2589211c8491b8c
flutterdevtaha <59686887+flutterdevtaha@users.noreply.github.com>
2022-09-24 14:43:54 +0300

    adding home api to repository and repository impl

--- lib/data/repository/repository_impl.dart
+++ lib/data/repository/repository_impl.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_app/data/data_sources/remote/remote_data_source.dart';
+import 'package:flutter_app/data/mappers/home_mappers/home_object_response_mapper.dart';
 import 'package:flutter_app/data/mappers/login_register_mappers/authentication_response_mapper.dart';
 import 'package:flutter_app/data/mappers/forgot_password_response_mappers/forgot_password_response_mapper.dart';
 import 'package:flutter_app/data/network/error_handler/api_internal_status.dart';
@@ -8,6 +9,7 @@ import 'package:flutter_app/data/network/error_handler/error_handler.dart';
 import 'package:flutter_app/data/network/error_handler/response_messages.dart';
 import 'package:flutter_app/data/network/internet_checker/network_info.dart';
 import 'package:flutter_app/data/network/models/register_request.dart';
+import 'package:flutter_app/domain/models/home_models/home_object.dart';
 import 'package:flutter_app/domain/models/login_register_models/authentication.dart';
 import 'package:flutter_app/data/network/models/loginrequest.dart';
 import 'package:flutter_app/data/network/models/failure.dart';
@@ -84,6 +86,33 @@ class RepositoryImpl implements Repository {
       // its connected to internet so we can call the api
       try {
         final response = await _remoteDataSource.register(registerRequest);
+        if (response.status == ApiInternalStatus.success) {
+          // success reutrn either right
+          return Right(response.toDomain());
+        } else {
+          // failure return either left business error
+          return Left(
+            Failure(
+              ApiInternalStatus.failure,
+              response.message ?? ResponseMessage.defaultError,
+            ),
+          );
+        }
+      } catch (error) {
+        return Left(ErrorHandler.handle(error).failure);
+      }
+    } else {
+      // its not connected to internet so return a failure
+      return Left(DataSource.noInternetConnection.getFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, HomeObject>> getHomeData() async {
+    if (await _networkInfo.isConnected) {
+      // its connected to internet so we can call the api
+      try {
+        final response = await _remoteDataSource.getHomeData();
         if (response.status == ApiInternalStatus.success) {
           // success reutrn either right
           return Right(response.toDomain());
--- lib/domain/repository/repository.dart
+++ lib/domain/repository/repository.dart
@@ -2,6 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_app/data/network/models/failure.dart';
 import 'package:flutter_app/data/network/models/loginrequest.dart';
 import 'package:flutter_app/data/network/models/register_request.dart';
+import 'package:flutter_app/domain/models/home_models/home_object.dart';
 import 'package:flutter_app/domain/models/login_register_models/authentication.dart';
 
 abstract class Repository {
@@ -10,4 +11,5 @@ abstract class Repository {
   Future<Either<Failure, Authentication>> register(
     RegisterRequest registerRequest,
   );
+  Future<Either<Failure, HomeObject>> getHomeData();
 }
